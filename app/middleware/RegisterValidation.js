15964a36a6a01911e587586f00faa687bbc38a25
Leonardo Trisna Agung <lagung@INDOSOFT.DC>
2023-03-21 13:19:01 +0700

    Register validation

--- app/middleware/RegisterValidation.js
+++ app/middleware/RegisterValidation.js
@@ -4,17 +4,17 @@ export const RegisterValidation = async (req, res, next) => {
 
     const { username, email, password, confPassword } = req.body;
 
-    const errors = {
-        username: false,
-        email: false
-    }
+    const errors = {};
     
-    if( await existName( username ) ) errors.username = 'Username already used';
-    if( await existEmail( email ) ) errors.email = "Email already used";
+    if(await existName(username)) errors['username'] = 'Username already used';
+    if(await existEmail(email)) errors['email'] = "Email already used";
+    if(username.length < 6) errors['username'] = "Username should atleast 6 char";
+    if(password !==confPassword) errors['password'] = "Password doesn't match";
+    if(password.length < 6) errors['password'] = "Your password to short";
 
-    if( errors.username === false && errors.email === false ) {
+    if(Object.keys(errors).length === 0) {
         next();
     } else {
-        res.send({ errors })
+        res.send({errors})
     }
 }
